8c150220b2ace554620b3e1462957830b201b09f
Colin Ciervo <colinciervo@@gmail.com>
2022-09-03 13:21:34 -0400

    Add more tests

--- test/sleep-test.js
+++ test/sleep-test.js
@@ -21,26 +21,52 @@ describe("Sleep", () => {
     })
 
     it("should be able to return a date", () => {
-
+        expect(userSleep.date).to.equal("2019/06/15")
     })
 
     it("should be able to return hours slept", () => {
-
+        expect(userSleep.hoursSlept).to.equal(6.1)
     })
 
     it("should be able to return the quality of sleep", () => {
-
+        expect(userSleep.sleepQuality).to.equal(2.2)
     })
 
-    it("shoukd be able to return the average hours of sleep per week", () => {
-
+    it("shoukd be able to return the average hours of sleep per day", () => {
+        const averageSleepPerDay = userSleep.getAverageSleepPerDay()
+        expect(averageSleepPerDay).to.equal(8)
     })
 
-    it("should be able to return the average sleep quality per week", () => {
+    it("should be able to return the average sleep quality per day", () => {
+        const averageQualitySleep = userSleep.getAverageQuality()
+        expect(averageQualitySleep).to.equal(2.6)
+    })
+    
+    it("should be able to return hours slept on a specific day", () => {
+        const sleepForADay = userSleep.getHoursSleptPerDay("2019/06/20")
+        expect(sleepForADay).to.equal(9.3)
+    })
 
+    it("should be able to return sleep quality for a specific day", () => {
+        const sleepQualityForADay = userSleep.getSleepQualityPerDay("2019/06/20")
+        expect(sleepQualityForADay).to.equal(1.2)
+    })
+
+    it("should be able to return the total hours slept in a week", () => {
+        const sleepForAWeek = userSleep.getSleepForTheWeek("2019/06/21")
+        expect(sleepForAWeek).to.deep.equal({
+            date: [
+                "2019/06/15",
+                "2019/06/16",
+                "2019/06/17",
+                "2019/06/18",
+                "2019/06/19",
+                "2019/06/20",
+                "2019/06/21",
+            ]
+        })
     })
 
     it("should return average sleep quality for all users", () => {
-
     })
 })
