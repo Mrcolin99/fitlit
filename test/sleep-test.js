a96f8145a72c5303c907957dfc996affbe970144
Colin Ciervo <colinciervo@@gmail.com>
2022-09-03 12:58:31 -0400

    Add test skeleton

--- test/sleep-test.js
+++ test/sleep-test.js
@@ -15,4 +15,32 @@ describe("Sleep", () => {
     it("should be an instance of Sleep", () => {
         expect(userSleep).to.be.an.instanceOf(Sleep)
     })
+
+    it("should be able to store an ID", () => {
+        expect(userSleep).to.equal(1)
+    })
+
+    it("should be able to return a date", () => {
+
+    })
+
+    it("should be able to return hours slept", () => {
+
+    })
+
+    it("should be able to return the quality of sleep", () => {
+
+    })
+
+    it("shoukd be able to return the average hours of sleep per week", () => {
+
+    })
+
+    it("should be able to return the average sleep quality per week", () => {
+
+    })
+
+    it("should return average sleep quality for all users", () => {
+
+    })
 })
