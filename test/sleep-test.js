515cc440a92771c07a7ee84562414627cdc82821
Colin Ciervo <colinciervo@@gmail.com>
2022-09-03 13:35:17 -0400

    Add fully functioning sleep test

--- test/sleep-test.js
+++ test/sleep-test.js
@@ -5,55 +5,55 @@ describe("Sleep", () => {
     let userSleep;
 
     beforeEach(() => {
-        userSleep = new Sleep(1, mockSleepData)
+        userSleep = new Sleep(1, mockSleepData);
     });
 
     it.skip("should be a function", () => {
-        expect(Sleep).to.be.a("function")
-    })
+        expect(Sleep).to.be.a("function");
+    });
 
     it.skip("should be an instance of Sleep", () => {
-        expect(userSleep).to.be.an.instanceOf(Sleep)
-    })
+        expect(userSleep).to.be.an.instanceOf(Sleep);
+    });
 
     it.skip("should be able to store an ID", () => {
-        expect(userSleep).to.equal(1)
-    })
+        expect(userSleep).to.equal(1);
+    });
 
     it.skip("should be able to return a date", () => {
-        expect(userSleep.date).to.equal("2019/06/15")
-    })
+        expect(userSleep.date).to.equal("2019/06/15");
+    });
 
     it.skip("should be able to return hours slept", () => {
-        expect(userSleep.hoursSlept).to.equal(6.1)
-    })
+        expect(userSleep.hoursSlept).to.equal(6.1);
+    });
 
     it.skip("should be able to return the quality of sleep", () => {
-        expect(userSleep.sleepQuality).to.equal(2.2)
-    })
+        expect(userSleep.sleepQuality).to.equal(2.2);
+    });
 
     it.skip("shoukd be able to return the average hours of sleep per day", () => {
-        const averageSleepPerDay = userSleep.getAverageSleepPerDay()
-        expect(averageSleepPerDay).to.equal(8)
-    })
+        const averageSleepPerDay = userSleep.getAverageSleepPerDay();
+        expect(averageSleepPerDay).to.equal(8);
+    });
 
     it.skip("should be able to return the average sleep quality per day", () => {
-        const averageQualitySleep = userSleep.getAverageQuality()
-        expect(averageQualitySleep).to.equal(2.6)
-    })
-    
+        const averageQualitySleep = userSleep.getAverageQuality();
+        expect(averageQualitySleep).to.equal(2.6);
+    });
+
     it.skip("should be able to return hours slept on a specific day", () => {
-        const sleepForADay = userSleep.getHoursSleptPerDay("2019/06/20")
-        expect(sleepForADay).to.equal(9.3)
-    })
+        const sleepForADay = userSleep.getHoursSleptPerDay("2019/06/20");
+        expect(sleepForADay).to.equal(9.3);
+    });
 
     it.skip("should be able to return sleep quality for a specific day", () => {
-        const sleepQualityForADay = userSleep.getSleepQualityPerDay("2019/06/20")
-        expect(sleepQualityForADay).to.equal(1.2)
-    })
+        const sleepQualityForADay = userSleep.getSleepQualityPerDay("2019/06/20");
+        expect(sleepQualityForADay).to.equal(1.2);
+    });
 
     it.skip("should be able to return the total hours slept in a week", () => {
-        const sleepForAWeek = userSleep.getSleepForTheWeek("2019/06/21")
+        const sleepForAWeek = userSleep.getSleepForTheWeek("2019/06/21");
         expect(sleepForAWeek).to.deep.equal({
             date: [
                 "2019/06/15",
@@ -73,9 +73,35 @@ describe("Sleep", () => {
                 9.3,
                 7.8
             ]
-        })
-    })
+        });
+    });
+
+    it.skip("should be able to return the total sleep quality for a week", () => {
+        const sleepQualityForTheWeek = userSleep.getQualityForTheWeek("2019/06/21");
+        expect(sleepQualityForTheWeek).to.deep.equal({
+            date: [
+                "2019/06/15",
+                "2019/06/16",
+                "2019/06/17",
+                "2019/06/18",
+                "2019/06/19",
+                "2019/06/20",
+                "2019/06/21"
+            ],
+            sleepQuality: [
+                2.2,
+                3.8,
+                2.6,
+                3.1,
+                1.2,
+                1.2,
+                4.2
+            ]
+        });
+    });
 
     it.skip("should return average sleep quality for all users", () => {
-    })
-})
+        const allUserQuality = userSleep.getAllQuality()
+        expect(allUserQuality).to.equal(2.6)
+    });
+});
