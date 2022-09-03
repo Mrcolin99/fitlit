7766e4d16478cb48baadef6533f50284205ab98e
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-09-03 18:59:57 -0500

    Clean up UserRepository-test.js

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -4,7 +4,6 @@ import UserRepository from '../src/UserRepository'
 describe('User Repository', () => {
   let repo;
 
-
   beforeEach(() => {
     repo = new UserRepository([{
       id: 1,
@@ -14,15 +13,18 @@ describe('User Repository', () => {
       strideLength: 4.3,
       dailyStepGoal: 10000,
       friends: [16, 4, 8]
-    }])
-  })
+    }]);
+  });
+
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
 
   });
+
   it('should be an instance of UserRepository', function () {
     expect(repo).to.be.an.instanceof(UserRepository)
-  })
+  });
+
   it('should have user data', function () {
     expect(repo.data).to.deep.equal([{
       id: 1,
@@ -32,10 +34,10 @@ describe('User Repository', () => {
       strideLength: 4.3,
       dailyStepGoal: 10000,
       friends: [16, 4, 8]
-    }])
-  })
+    }]);
+  });
 
   it('should have an average step goal', function () {
     expect(repo.avgStepGoal()).to.equal(10000)
-  })
+  });
 });
