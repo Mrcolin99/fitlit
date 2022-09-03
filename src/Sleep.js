ffb3734ea0a74f622384572d62d0634beadf1119
Jeffrey Cook <cookturingedu@gmail.com>
2022-09-03 14:05:59 -0400

    Add sleptHours property to sleep class

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,7 +1,8 @@
 class Sleep{
   constructor(id, sleepData) {
     this.userID = id;
-    this.date = sleepData[0].date
+    this.date = sleepData[0].date;
+    this.hoursSlept = sleepData[0].hoursSlept;
   }
 }
 
