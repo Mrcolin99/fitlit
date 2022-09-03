ae127afe15cd9304974afed16d84183a10fbdf39
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-09-03 18:48:31 -0500

    Clean up scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,6 @@
 // Imports
 import './css/styles.css';
-import './images/turing-logo.png'
+import './images/turing-logo.png';
 import UserRepository from './UserRepository';
 import User from './User';
 import apiCalls from './apiCalls';
@@ -17,8 +17,8 @@ const userFriends = document.querySelector("#user-info-friends");
 const userWelcome = document.querySelector("#nav-user-name");
 const userStepsAverage = document.querySelector("#user-step-average");
 const overallStepsAverage = document.querySelector("#step-goal-average");
-const todayWater = document.querySelector("#user-hydration-daily");
-const weeksWater = document.querySelector("#user-hydration-weekly");
+const userWaterDaily = document.querySelector("#user-hydration-daily");
+const userWaterWeekly = document.querySelector("#user-hydration-weekly");
 const userSleptDaily = document.querySelector("#user-slept-daily");
 const userQualityDaily = document.querySelector("#user-quality-daily");
 const userSleptWeekly = document.querySelector("#user-slept-weekly");
@@ -76,7 +76,7 @@ function displayUserCard() {
   userEmail.innerHTML = `Email: ${user.email}`;
   userStride.innerHTML = `Stride: ${user.strideLength}`;
   userFriends.innerHTML = `Friends: ${user.friends}`;
-}
+};
 
 function showFirstName() {
   userWelcome.innerHTML = `Welcome ${user.returnFirstName()}`;
@@ -85,21 +85,21 @@ function showFirstName() {
 function compareStepGoal() {
   userStepsAverage.innerHTML = `Daily Step Goal: ${user.dailyStepGoal}`;
   overallStepsAverage.innerHTML = `Overall Step Goal: ${userRepo.avgStepGoal()}`;
-}
+};
 
 function waterForTheDay() {
   hydration.setUserHydrateData(hydrationData)
   const waterToday = hydration.returnOuncesByDate(hydration.date);
-  todayWater.innerHTML = `Todays Hydration: ${waterToday}oz.`;
-}
+  userWaterDaily.innerHTML = `Todays Hydration: ${waterToday}oz.`;
+};
 
 function waterForTheWeek() {
   const weeklyIntake = hydration.returnOuncesByWeek(hydration.date);
-  weeksWater.innerHTML = 'Weekly Intake: <br/>'
+  userWaterWeekly.innerHTML = 'Weekly Intake: <br/>';
   weeklyIntake.date.forEach((value, i) => {
-    weeksWater.innerHTML += `Date: ${value}, Amount: ${weeklyIntake.numOunces[i]}oz. <br/>`
+    userWaterWeekly.innerHTML += `Date: ${value}, Amount: ${weeklyIntake.numOunces[i]}oz. <br/>`;
   });
-}
+};
 
 function showDailyHoursSlept() {
   let dailyHours = sleep.getHoursSleptPerDay(sleep.date);
@@ -109,33 +109,33 @@ function showDailyHoursSlept() {
 function showDailyQuality() {
   let dailyQuality = sleep.getSleepQualityPerDay(sleep.date);
   userQualityDaily.innerHTML = `Today's Sleep Quality: ${dailyQuality}`;
-}
+};
 
 function showWeeklyHoursSlept() {
   const weeklySleep = sleep.getSleepForTheWeek(sleep.date);
-  userSleptWeekly.innerHTML = `This Week's Hours Slept: <br/>`
+  userSleptWeekly.innerHTML = `This Week's Hours Slept: <br/>`;
   weeklySleep.date.forEach((value, i) => {
-    userSleptWeekly.innerHTML += `Date: ${value}, Hours: ${weeklySleep.hoursSlept[i]} <br/>`
-  })
-}
+    userSleptWeekly.innerHTML += `Date: ${value}, Hours: ${weeklySleep.hoursSlept[i]} <br/>`;
+  });
+};
 
 function showWeeklyQuality() {
   const weeklySleepQuality = sleep.getQualityForTheWeek(sleep.date);
-  userQualityWeekly.innerHTML = `This Week's Sleep Quality: <br/>`
+  userQualityWeekly.innerHTML = `This Week's Sleep Quality: <br/>`;
   weeklySleepQuality.date.forEach((value, i) => {
-    userQualityWeekly.innerHTML += `Date: ${value}, Quality: ${weeklySleepQuality.sleepQuality[i]} <br/>`
-  })
-}
+    userQualityWeekly.innerHTML += `Date: ${value}, Quality: ${weeklySleepQuality.sleepQuality[i]} <br/>`;
+  });
+};
 
 function showAllTimeHoursSlept() {
   const allTimeHours = sleep.getAllHours();
   userSleptAllTime.innerHTML = `All Times Hours Slept: ${allTimeHours}`;
-}
+};
 
 function showAllTimeQuality() {
   const allTimeQuality = sleep.getAllQuality();
   userQualityAllTime.innerHTML = `All Time Sleep Quality: ${allTimeQuality}`;
-}
+};
 
 // Event Listeners
 window.addEventListener("load", fetchApiCalls("load"));
