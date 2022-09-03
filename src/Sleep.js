7a60299147b8bb3ad0b1914ce49279f116258df1
Jeffrey Cook <cookturingedu@gmail.com>
2022-09-03 14:55:52 -0400

    Add method to calculate average of sleep hours per day

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,10 +1,35 @@
+import sleepData from "./data/sleep-data";
+
 class Sleep{
   constructor(id, sleepData) {
     this.userID = id;
+    this.userSleepData = this.getUserSleepData(sleepData);
     this.date = sleepData[0].date;
     this.hoursSlept = sleepData[0].hoursSlept;
-    this.sleepQuality = sleepData[0].sleepQuality
+    this.sleepQuality = sleepData[0].sleepQuality;
+    this.sleepData = sleepData;
   }
-}
+
+  getUserSleepData(sleepData) {
+    const userSleep = sleepData.filter(userSleepData => {
+      if(userSleepData.userID === this.userID) {
+        return userSleepData;
+      }
+    });
+    userSleep.reverse();
+    return userSleep;
+  };
+
+  getAverageSleepPerDay() {
+    const sleepPerDay = this.sleepData.map(sleep => {
+      return sleep.hoursSlept;
+    })
+    const sleepAvgPerDay = sleepPerDay.reduce((totalSleep, currentSleepHours) => {
+      return totalSleep + currentSleepHours
+    }, 0)
+    return Math.round(sleepAvgPerDay / this.sleepData.length);
+  };
+
+};
 
 export default Sleep;
