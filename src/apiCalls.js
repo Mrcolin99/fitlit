4ada15eece9c09434b6fbeb3a616b7c34796a24f
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-09-03 18:50:04 -0500

    Clean up apiCalls.js

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,3 @@
-// Set up function to accept an endpoint path as an argument
 const fetchApiUrl = (path) => {
   return fetch(`https://fitlit-api.herokuapp.com/api/v1/${path}`)
   .then(response => response.json())
@@ -8,16 +7,11 @@ const fetchApiUrl = (path) => {
 
 
 const fetchData = () => {
-  // Represent the eventual completion or failure of our fetch call
-  // Promise.all() accepts an iterable (such as an array) as an argument
   return Promise.all([
-    // Invoke the fetchApiUrl function
-    // Dynamically pass in each URL path as an argument
     fetchApiUrl("users"),
     fetchApiUrl("hydration"),
     fetchApiUrl("sleep"),
   ]);
 };
 
-// Export the data that returns from the response of the promise object
 export default { fetchData };
