e5b30fc7bc57d538d30e5b99a4461a7755deb58a
Colin Ciervo <colinciervo@@gmail.com>
2022-09-13 16:06:19 -0400

    Add findUserData method to the userRepository test

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -37,6 +37,19 @@ describe('User Repository', () => {
     }]);
   });
 
+  it("should return a single users data", () => {
+    const oneUsersData = repo.findUsersData(1)
+    expect(oneUsersData).to.deep.equal({
+      id: 1,
+      name: "Luisa Hane",
+      address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      email: "Diana.Hayes1@hotmail.com",
+      strideLength: 4.3,
+      dailyStepGoal: 10000,
+      friends: [16, 4, 8]
+    });
+  });
+
   it('should have an average step goal', function () {
     expect(repo.avgStepGoal()).to.equal(10000)
   });
